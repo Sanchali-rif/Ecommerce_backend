3cde15f0c54670b2f29ae84c5d5cbab223c6fc1a
Sanchali-rif <sanchalisaha05@gmail.com>
2026-08-06 20:48:08 +0530

    feat: define categories and items schemas in item model

--- models/item.model.js
+++ models/item.model.js
@@ -1,17 +1,50 @@
 import mongoose from "mongoose";
 
-const categories = mongoose.Schema({
+const categories = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    totalItem: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-});
-
-
-
-const items = mongoose.Schema({
-
-
-});
-
+const items = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    stock:{
+        type:Number,
+        default:0
+    },
+    image:{
+        type:String,
+        default:""
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CATEGORY",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Categories = mongoose.model("CATEGORY", categories);
 export const Items = mongoose.model("ITEM", items);
