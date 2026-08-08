80abd1886e9df3cabf1d1b4ab54980de21d6ef5f
Sanchali-rif <sanchalisaha05@gmail.com>
2026-08-08 23:05:08 +0530

    feat: add retailer routes to the main application

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ import express from "express";
 import dbconnect from "./config/db.js";
 import { userRouter } from "./routes/user.route.js";
 import { dummyRouter } from "./routes/dummy.route.js";
+import retailerRouter from "./routes/retailer.routes.js";
+
 const app = express();
 import "dotenv/config";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use("/api/users", userRouter);
 app.use("/api/dummy", dummyRouter);
+app.use("/api/retailer", retailerRouter);
 
 app.get("/health",(req,res)=>{
     res.json({
